d7a7e13c5d26c7d1afe9a78b51196bb71cb77ce6
rbjdj <reireijericho@gmail.com>
2025-07-10 15:08:53 +0800

    Update data analysis.py

--- data analysis.py
+++ data analysis.py
@@ -5,24 +5,21 @@ import matplotlib.pyplot as plt
 
 class DataAnalyzer:
     def __init__(self, mood_entries, journal_entries):
-        self.mood_entries = mood_entries  # List of dicts: {'date': ..., 'mood': ...}
-        self.journal_entries = journal_entries  # List of dicts: {'date': ..., 'text': ...}
+        self.mood_entries = mood_entries  
+        self.journal_entries = journal_entries 
 
     def mood_trend(self):
-        # Group moods by date
         daily_moods = defaultdict(list)
         for entry in self.mood_entries:
             date = entry['date']
             mood = entry['mood']
             daily_moods[date].append(mood)
 
-        # Calculate average mood per day
         avg_daily_mood = {
             date: sum(moods) / len(moods)
             for date, moods in daily_moods.items()
         }
 
-        # Plot mood trend
         dates = sorted(avg_daily_mood.keys())
         mood_scores = [avg_daily_mood[date] for date in dates]
 
@@ -38,12 +35,10 @@ class DataAnalyzer:
         return avg_daily_mood
 
     def journal_frequency(self):
-        # Count journal entries by date
         frequency = defaultdict(int)
         for entry in self.journal_entries:
             frequency[entry['date']] += 1
 
-        # Plot journal frequency
         dates = sorted(frequency.keys())
         counts = [frequency[date] for date in dates]
 
@@ -59,7 +54,6 @@ class DataAnalyzer:
         return frequency
 
     def wellness_score(self):
-        # Combine mood and journaling data to generate a wellness score
         score_by_day = defaultdict(lambda: {"moods": [], "journals": 0})
         for m in self.mood_entries:
             score_by_day[m['date']]['moods'].append(m['mood'])
@@ -67,14 +61,12 @@ class DataAnalyzer:
         for j in self.journal_entries:
             score_by_day[j['date']]['journals'] += 1
 
-        # Compute score: mood average + journal impact (e.g., +0.5 per entry)
         wellness_scores = {}
         for date, values in score_by_day.items():
             mood_avg = statistics.mean(values['moods']) if values['moods'] else 0
             journal_bonus = 0.5 * values['journals']
             wellness_scores[date] = round(mood_avg + journal_bonus, 2)
 
-        # Plot wellness score
         dates = sorted(wellness_scores.keys())
         scores = [wellness_scores[date] for date in dates]
 
